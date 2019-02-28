aeb66cd7a4b72e55fd14e064a9ce653d9cceb2b4
Seray Uzgur <serayuzgur@gmail.com>
2019-02-28 12:08:03 +0300

    poly and owned indexes

--- dbutil/models.go
+++ dbutil/models.go
@@ -6,16 +6,16 @@ type Model struct {
 	ID        uint `gorm:"primary_key"`
 	CreatedAt *time.Time
 	UpdatedAt *time.Time
-	DeletedAt *time.Time `sql:"index"`
+	DeletedAt *time.Time `gorm:"index"`
 }
 
 // PolymorphicModel helps to make your model having a polymorphic relation.
 type PolymorphicModel struct {
-	HolderID   uint
-	HolderType string
+	HolderID   uint   `gorm:"index;not null"`
+	HolderType string `gorm:"index;not null"`
 }
 
 // OwnedModel helps to make your model having a owner restriction
 type OwnedModel struct {
-	OwnerID uint
+	OwnerID uint `gorm:"index;not null"`
 }
