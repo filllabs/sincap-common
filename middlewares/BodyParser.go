2bc22ef1ff70e0d514d6d633c62650c362599b5c
Seray Uzgur <serayuzgur@gmail.com>
2022-09-09 15:06:00 +0300

    n new in Body parser fixed

--- middlewares/BodyParser.go
+++ middlewares/BodyParser.go
@@ -3,8 +3,8 @@ package middlewares
 import "github.com/gofiber/fiber/v2"
 
 func BodyParser[T any](key string) func(ctx *fiber.Ctx) error {
-	t := new(T)
 	return func(ctx *fiber.Ctx) error {
+		t := new(T)
 		err := ctx.BodyParser(t)
 		if err != nil {
 			return err
