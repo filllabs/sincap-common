2e58bdf816863a0f543bd9fcf7ef03e9583bb697
Seray Uzgur <serayuzgur@gmail.com>
2022-09-21 15:08:16 +0300

    BodyParserMap with forbidden field support arrived

--- middlewares/BodyParser.go
+++ middlewares/BodyParser.go
@@ -1,6 +1,10 @@
 package middlewares
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func BodyParser[T any](key string) func(ctx *fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
@@ -10,6 +14,22 @@ func BodyParser[T any](key string) func(ctx *fiber.Ctx) error {
 			return err
 		}
 		ctx.Locals(key, t)
+		return ctx.Next()
+	}
+}
+func BodyParserMap(key string, forbiddenFields ...string) func(ctx *fiber.Ctx) error {
+	return func(ctx *fiber.Ctx) error {
+		t := make(map[string]any, 1)
+		err := ctx.BodyParser(&t)
+		if err != nil {
+			return err
+		}
+		for _, key := range forbiddenFields {
+			if _, ok := t[key]; ok {
+				return ctx.Status(422).JSON(map[string]string{"error": fmt.Sprintf("You cannot update %s Field", key)})
+			}
+		}
+		ctx.Locals(key, &t)
 		return ctx.Next()
 	}
 }
